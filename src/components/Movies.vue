db477607969627c8ef7e54836c0c064f6f79d3f2
LAPTOP-VE3N82M9\Gerve <gerveld23@gmail.com>
2021-05-10 11:56:28 -0500

    Adding to the Movies.Vue

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
       <h1>Upcoming Movies</h1>
-
+        <p v-for="movie in movies" :key="movie.id">{{movies.titles}}</p>
  </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
-    name:'Movies'
+    name:'Movies',
+    data()
+    {
+        return{
+            movies:[]
+        }
+    },
+    mounted()
+    {
+        acios.get("https://api.themoviedb.org/3/movie/latest?api_key=4e44efca8c5b5ab2f9bdba7250e3b882&language=en-US")
+        .then( response => {
+            console.log(response);
+            this.movies = response.data.results;
+        })
+    }
 
 }
 </script>
