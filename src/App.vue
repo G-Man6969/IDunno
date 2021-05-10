3eac931084f6bc6e40157ff8477dd9c3ff22229c
LAPTOP-VE3N82M9\Gerve <gerveld23@gmail.com>
2021-05-10 11:40:10 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Movies></Movies>
+   
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+import Movies from './components/Movies.vue'
+
+export default {
+  name: 'App',
+  components: {
+   Nav,
+   Movies
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ 
+}
+</style>
