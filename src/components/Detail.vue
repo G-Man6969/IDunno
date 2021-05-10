c815cb51407109e4aa2baaa64ffbf6dcd38c075b
LAPTOP-VE3N82M9\Gerve <gerveld23@gmail.com>
2021-05-10 12:56:07 -0500

    hello again

---
+++ src/components/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>Movie Detail</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Detail',
+    data(){
+        return{
+            id:""
+        }
+    },
+    mounted()
+    {
+        axios.get("https://api.themoviedb.org3/movie/{movie_id}?api_key")
+    }
+}
+</script>
+
+<style>
+
+</style>
